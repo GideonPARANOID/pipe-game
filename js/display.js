e96d743474928c7dc4e5eae9f16d73edf1cbb653
gideonparanoid <gideon.jones@ntlworld.com>
2014-06-04 10:12:21 +0100

    block type assignment now with crude weighting algorithm

--- js/display.js
+++ js/display.js
@@ -16,12 +16,18 @@ function DOM(game, id) {
       'width' : ((game.size * 60) + 10) + 'px'
    });
 
+console.log(game.getBlockState(0, 0).unshift('block'));
+
    // initialising grid
    for (var x = 0; x < game.size; x++) {
       for (var y = 0; y < game.size; y++) {
+
+         var classes = game.getBlockState(x, y);
+         classes.unshift('block');
+
          this.grid.append($('<div />').attr({
             'id'     : x + '-' + y,
-            'class'  : 'block' + game.getBlockState(x, y)
+            'class'  : classes.join(' ')
          }));
       }
    }
--- js/game.js
+++ js/game.js
@@ -13,6 +13,13 @@ function Game(size, speed) {
    this.grid = [];
    this.speed = speed;
 
+   // where the liquid enters & exists the game, clockwise from origin (this.size * 4)
+   this.start = 0;
+   this.end = 0;
+
+   // current block
+   this.current = 0;
+
    for (var x = 0; x < size; x++) {
       this.grid[x] = [];
 
@@ -32,13 +39,14 @@ Game.prototype.getBlockState = function(x, y) {
  * a grid square
  */
 function Block(type) {
-   this.type = 0;
+   this.type = randomType();
    this.revealed = false;
    this.used = false;
    this.rotation = 0;
 }
 
-// block types
+// block modifiers
+
 Block.STRAIGHT = 'straight';
 Block.BEND = 'bend';
 Block.SLOW = 'slow';
@@ -46,8 +54,54 @@ Block.FAST = 'fast';
 Block.BREAK = 'break';
 Block.ALARM = 'alarm';
 
+Block.REVEALED = 'revealed'
+Block.USED = 'used';
+Block.ROTATION = [
+   'rotation000',
+   'rotation090',
+   'rotation180',
+   'rotation270'
+]
 
 
+/**
+ * @return  array    string constants describing the the state
+ */
 Block.prototype.getState = function() {
-   return '';
+   var state = [this.type, Block.ROTATION[this.rotation]];
+   
+   switch (true) {
+      case this.revealed : 
+         state.push(Block.REVEALED);
+      case this.used :
+         state.push(Block.USED);
+   }
+
+   return state;
 }
+
+
+
+/**
+ * @param   weight   object with keys of block constants & values of each type's weight
+ * @return  string   constant describing a type
+ */
+function randomType() {  
+   var weights = [];
+   weights[Block.STRAIGHT] = 2;
+   weights[Block.BEND] = 3;
+   weights[Block.SLOW] = 1;
+   weights[Block.FAST] = 0;
+   weights[Block.BREAK] = 1;
+   weights[Block.ALARM] = 0;
+
+   var weightedTypes = [];
+   for (var key in weights) {
+      for (var i = 0; i < weights[key]; i++) {
+         weightedTypes.push(key);
+      }
+   }
+
+   return weightedTypes[Math.floor(Math.random() * weightedTypes.length)];
+}
+
