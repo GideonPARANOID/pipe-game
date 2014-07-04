828e7bc54ac04c0752f3ec4d7e7026319c3f93c8
gideonparanoid <gideon.jones@ntlworld.com>
2014-07-04 22:43:24 +0100

    that  fixed \this\ scope issue

--- js/display.js
+++ js/display.js
@@ -18,26 +18,36 @@ function DOM(id, size) {
    for (var x = 0; x < size; x++) {
       for (var y = 0; y < size; y++) {
 
-         var classes = game.getBlockState(x, y);
+         var _this = this, classes = game.getBlockState(x, y);
          classes.unshift('block');
 
-
          this.grid.append($('<div />').attr({
-            'id'     : x + '-' + y,
-            'class'  : classes.join(' ')
-         }).on('click', this.click));
+               'id'     : x + '-' + y,
+               'class'  : classes.join(' ')
+            }).on('click', function(event) {
+               _this.click(event);
+         }));
       }
    }
 }
 
 
+/**
+ * @param   event    click event object
+ * activates a block & rerenders it based on its state
+ */
 DOM.prototype.click = function(event) {
-   var indexes = $(this).attr('id').split('-');
+   var indexes = $(event.target).attr('id').split('-');
    game.activateBlock(indexes[0], indexes[1]);   
    this.renderBlock(indexes[0], indexes[1]);
 }
 
 
+/**
+ * @param   x        coordinate of the block in the grid 
+ * @param   y        coordinate of the block in the grid 
+ * renders a block based on its state
+ */
 DOM.prototype.renderBlock = function(x, y) {
    var classes = game.getBlockState(x, y);
    classes.unshift('block');
